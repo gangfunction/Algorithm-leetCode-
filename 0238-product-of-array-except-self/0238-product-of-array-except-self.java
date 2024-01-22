8ef462cfbc95bff5889b9f3efb1d86844eacfc3a
Lee Gangju <gangfunction@gmail.com>
2024-01-22 20:36:16 +0900

    Time: 2 ms (71.30%), Space: 53.3 MB (45.18%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,19 +1,17 @@
 class Solution {
+    /*
+    문제는 곱셈을 해야하므로 곱셈을 전의것만 할수밖에 없으니까, 곱셈의 순서를 좌의 곱과 우의곱으로 나눈다는것.
+    */
     public int[] productExceptSelf(int[] nums) {
-        int N= nums.length;
-        int[] left_products = new int[N];
-        int[] right_products = new int[N];
+        int N= nums.length, R =1;
         int[] output_arr = new int[N];
-        
-        left_products[0]=1;
-        right_products[N-1]=1;
-        
-        for(int i =1; i<N;i++){
-            left_products[i] = nums[i-1]*left_products[i-1];
-            right_products[N-i-1] = nums[N-i]*right_products[N-i];
+        output_arr[0]=1;
+        for(int i=1; i<N; i++ ){
+            output_arr[i]=nums[i-1]*output_arr[i-1];
         }
-        for(int i=0; i<N; i++){
-            output_arr[i] =  left_products[i]*right_products[i];
+        for(int i=N-1; i>=0; i--){
+            output_arr[i] = output_arr[i]*R;
+            R= R*nums[i];
         }
         return output_arr;
         
