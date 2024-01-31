1abca9746bdbf2929a35a34bc521b4c229ee4e66
Lee Gangju <gangfunction@gmail.com>
2024-02-01 01:42:55 +0900

    Time: 10 ms (97.60%), Space: 112.3 MB (85.69%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,42 +1,46 @@
 class Solution {
     private int[] parent;
-    private int[] size; // Size array to keep track of the size of each set
-
-    private int find(int x) {
-        while (x != parent[x]) {
-            parent[x] = parent[parent[x]]; // Path compression
-            x = parent[x];
-        }
-        return x;
-    }
-
-    private void union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-        if (rootX != rootY) {
-            // Weighted union
-            if (size[rootX] < size[rootY]) {
-                parent[rootX] = rootY;
-                size[rootY] += size[rootX];
-            } else {
-                parent[rootY] = rootX;
-                size[rootX] += size[rootY];
-            }
-        }
-    }
+    private int[] size;
 
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         parent = new int[n];
         size = new int[n];
+
         for (int i = 0; i < n; i++) {
             parent[i] = i;
-            size[i] = 1; // Initialize size for each node as 1
+            size[i] = 1;
         }
 
         for (int[] edge : edges) {
-            union(edge[0], edge[1]);
+            int x = edge[0];
+            int y = edge[1];
+
+            // Union with path compression
+            while (x != parent[x]) {
+                parent[x] = parent[parent[x]];
+                x = parent[x];
+            }
+            while (y != parent[y]) {
+                parent[y] = parent[parent[y]];
+                y = parent[y];
+            }
+
+            // Union by size
+            if (x != y) {
+                if (size[x] < size[y]) {
+                    parent[x] = y;
+                    size[y] += size[x];
+                } else {
+                    parent[y] = x;
+                    size[x] += size[y];
+                }
+            }
         }
 
-        return find(source) == find(destination);
+        // Final find with path compression
+        while (source != parent[source]) source = parent[source];
+        while (destination != parent[destination]) destination = parent[destination];
+
+        return source == destination;
     }
 }
