df34245821762a5f75f040c0d758c4eff234ceea
Lee Gangju <gangfunction@gmail.com>
2024-02-01 01:40:45 +0900

    Time: 13 ms (92.26%), Space: 112.2 MB (86.35%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,9 +1,10 @@
 class Solution {
     private int[] parent;
+    private int[] rank;
 
     private int find(int x) {
         if (parent[x] != x) {
-            parent[x] = find(parent[x]);
+            parent[x] = find(parent[x]); // 경로 압축
         }
         return parent[x];
     }
@@ -12,20 +13,27 @@ class Solution {
         int rootX = find(x);
         int rootY = find(y);
         if (rootX != rootY) {
-            parent[rootX] = rootY;
+            // 랭크를 이용한 합병
+            if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
         }
     }
 
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         parent = new int[n];
+        rank = new int[n];
         for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
 
         for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            union(u, v);
+            union(edge[0], edge[1]);
         }
 
         return find(source) == find(destination);
