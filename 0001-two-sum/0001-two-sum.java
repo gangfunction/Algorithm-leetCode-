730d270268795e4c4910f2fac0cb3fc8fd579311
Lee Gangju <gangfunction@gmail.com>
2024-03-05 13:20:34 +0900

    Time: 2 ms (98.02%), Space: 45 MB (20.50%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,29 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        boolean flag = false;
-        int[] answer = new int[2];
-        int[] answer2= new int[2];
-        List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
-        for(int num : nums){
-            if(target/2 != num){
-                int b = target - num;
-                int c =list.indexOf(b);   
-                if( c != -1){
-                int d = list.indexOf(num);            
-                list.remove(list.indexOf(num));                
-                answer[0]=d;
-                answer[1]=c;
-                flag = true;
+    Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] {
+                    map.get(complement), i 
+                    };
                 }
-            }
+            map.put(nums[i], i);
         }
-        int a = target/2;
-        answer2[0] = list.indexOf(a);
-        answer2[1] = list.lastIndexOf(a);
-            
-
-        return flag? answer :answer2 ;
-
-        
+        return null;
     }
+
+
 }
