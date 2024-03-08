c6605285a0de45d84c6ebf3fdf55304125d130e2
Lee Gangju <gangfunction@gmail.com>
2024-03-08 12:20:20 +0900

    Time: 2 ms (99.66%), Space: 43 MB (73.52%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -3,20 +3,21 @@ class Solution {
         Arrays.sort(candidates);
         List<List<Integer>> result = new ArrayList<>();
         backtrack(result, new ArrayList<>(), candidates, target, 0);
-
         return result;
     }
+    
     private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] candidates, int remain, int start) {
-        if (remain <0) return; // 목표값을 초과한 경우
-        else if (remain == 0) { // 목표값에 도달한경우
+        if (remain == 0) { // 목표값에 도달한경우
             result.add(new ArrayList<>(tempList));
-        } else {
-            for (int i = start; i <= candidates.length-1; i++) {
-                if(i>start && candidates[i] == candidates[i-1]) continue;
-                tempList.add(candidates[i]);
-                backtrack(result, tempList, candidates, remain - candidates[i], i+1); 
-                tempList.remove(tempList.size() - 1); // 백트래킹
-            }
+            return;
+        }
+        
+        for (int i = start; i <= candidates.length-1; i++) {
+            if(remain<candidates[i]) break;
+            if(i>start && candidates[i] == candidates[i-1]) continue;
+            tempList.add(candidates[i]);
+            backtrack(result, tempList, candidates, remain - candidates[i], i+1);
+            tempList.remove(tempList.size() - 1); 
         }
     }   
 }
