0a3e4cc1ef851b7472bb4eb063752c1190e4b3ec
Lee Gangju <gangfunction@gmail.com>
2024-03-08 11:39:04 +0900

    Time: 0 ms (100.00%), Space: 41.5 MB (6.14%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -2,7 +2,6 @@ class Solution {
     /*
     backtracking?
     n is target
-    only unique number is used [1,2,3,4,5,6,7,8,9]
     case is distinguished by valid and invalid.
     */
     public List<List<Integer>> combinationSum3(int k, int n) {
@@ -12,14 +11,14 @@ class Solution {
     }
     
     private void backtrack(List<List<Integer>> result, List<Integer> tempList, int k, int remain, int start) {
-        if (tempList.size() > k) return; // 더 이상 탐색할 필요가 없는 경우
+        if (tempList.size() > k) return; 
         else if (tempList.size() == k && remain == 0) {
-            result.add(new ArrayList<>(tempList)); // 유효한 조합 찾음
+            result.add(new ArrayList<>(tempList)); 
         } else {
             for (int i = start; i <= 9; i++) {
                 tempList.add(i);
-                backtrack(result, tempList, k, remain - i, i + 1); // 다음 숫자 탐색
-                tempList.remove(tempList.size() - 1); // 백트래킹
+                backtrack(result, tempList, k, remain - i, i + 1); 
+                tempList.remove(tempList.size() - 1); 
             }
         }
     }
