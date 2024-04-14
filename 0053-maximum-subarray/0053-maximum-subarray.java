34110436fab7ff357bd7a92b32d6c14c1c0c01c7
Lee Gangju <gangfunction@gmail.com>
2024-04-14 14:05:21 +0900

    Time: 1 ms (99.43%), Space: 57.2 MB (19.47%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -8,32 +8,47 @@ class Solution {
         3. 반복
     */
     public int maxSubArray(int[] nums) {
-        if(nums.length ==0){
-            return 0;
+        if (nums == null || nums.length == 0) {
+            return 0; // 배열이 비어있거나 null인 경우 처리
         }
-        return maxSubArrayCalculator(nums, 0, nums.length-1);
+
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
     }
-    private int maxSubArrayCalculator(int[] nums,int left, int right){
-        if(left>right){
-            return Integer.MIN_VALUE;
-        }
-        int mid = left + (right-left)/2;
-        int leftMax = maxSubArrayCalculator(nums, left, mid-1);
-        int rightMax= maxSubArrayCalculator(nums, mid+1, right);
+//     public int maxSubArray(int[] nums) {
+//         if(nums.length ==0){
+//             return 0;
+//         }
+//         return maxSubArrayCalculator(nums, 0, nums.length-1);
+//     }
+//     private int maxSubArrayCalculator(int[] nums,int left, int right){
+//         if(left>right){
+//             return Integer.MIN_VALUE;
+//         }
+//         int mid = left + (right-left)/2;
+//         int leftMax = maxSubArrayCalculator(nums, left, mid-1);
+//         int rightMax= maxSubArrayCalculator(nums, mid+1, right);
         
-        int leftSum =0, rightSum=0;
-        int sum=0;
+//         int leftSum =0, rightSum=0;
+//         int sum=0;
         
-        for(int i = mid-1; i>=left ; i--){
-            sum+= nums[i];
-            leftSum = Math.max(leftSum, sum);
-        }
-        sum =0;
-        for(int i = mid+1; i<= right; i++){
-            sum += nums[i];
-            rightSum = Math.max(rightSum,sum);    
-        }
-        int midMax = nums[mid] + leftSum+ rightSum;
-        return Math.max(midMax, Math.max(leftMax, rightMax));
-    }
+//         for(int i = mid-1; i>=left ; i--){
+//             sum+= nums[i];
+//             leftSum = Math.max(leftSum, sum);
+//         }
+//         sum =0;
+//         for(int i = mid+1; i<= right; i++){
+//             sum += nums[i];
+//             rightSum = Math.max(rightSum,sum);    
+//         }
+//         int midMax = nums[mid] + leftSum+ rightSum;
+//         return Math.max(midMax, Math.max(leftMax, rightMax));
+//     }
 }
