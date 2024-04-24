79b36dac4f6173ccc487599dccc1d5754df2116b
Lee Gangju <gangfunction@gmail.com>
2024-04-24 10:14:10 +0900

    Time: 8 ms (76.13%), Space: 44.5 MB (40.20%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,35 +1,26 @@
 class Solution {
-    // + is not used for unary operation
-    // - is not used for unary operation
-    
     public int calculate(String s) {
-        Stack<Integer> stack = new Stack<>();
-        int operand = 0;
-        int result = 0; 
-        int sign = 1;  
+        int operand = 0, result = 0, sign = 1;
+        LinkedList<Integer> stack = new LinkedList<>();
 
-        for (int i = 0; i < s.length(); i++) { 
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
 
             if (Character.isDigit(ch)) {
-                operand = 10 * operand + (int) (ch - '0');
-            } else if (ch == '+') {
+                operand = 10 * operand + (ch - '0');
+            } else if (ch == '+' || ch == '-') {
                 result += sign * operand;
-                sign = 1;
-                operand = 0;
-            } else if (ch == '-') {
-                result += sign * operand;
-                sign = -1;
+                sign = (ch == '+') ? 1 : -1;
                 operand = 0;
             } else if (ch == '(') {
                 stack.push(result);
                 stack.push(sign);
-                sign = 1;   
                 result = 0;
+                sign = 1;
             } else if (ch == ')') {
-                result += sign * operand;  
-                result *= stack.pop(); 
-                result += stack.pop(); 
+                result += sign * operand;
+                result *= stack.pop();
+                result += stack.pop();
                 operand = 0;
             }
         }
