5c6f224a810b18f0746330903a68594fef504df1
Lee Gangju <gangfunction@gmail.com>
2024-06-06 21:13:08 +0900

    Time: 2199 ms (5.11%), Space: 60.4 MB (30.64%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,27 +1,50 @@
+import java.util.Random;
+
 class Solution {
+    private static final Random rand = new Random();
+
     public int findKthLargest(int[] nums, int k) {
-    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-    
-    for (int num : nums) {
-        minHeap.offer(num);
-        
-        if (minHeap.size() > k) {
-            minHeap.poll(); 
-        }
-    }
-    return minHeap.poll(); 
+        // k번째 큰 요소를 찾기 위해 k를 변환
+        int indexToFind = nums.length - k;
+        return quickselect(nums, 0, nums.length - 1, indexToFind);
     }
 
-  }
-    //         if(k==1){
-//             Arrays.sort(nums, Collections.reverseOrder());
-//             return nums[0];
-//         }
-//         int[] target= Arrays.copyOf(nums);        
-//         for(int i=0; i<k-1; i++){
-//             int num = target.length;
-//             Arrays.sort(target, Collections.reverseOrder());
-//             Arrays.sort(target, 1, num-1);
-            
-//         }
-//         return target[0];
+    private static int quickselect(int[] nums, int left, int right, int k) {
+        if (left == right) {
+            return nums[left];
+        }
+
+        int pivotIndex = partition(nums, left, right);
+
+        if (k == pivotIndex) {
+            return nums[k];
+        } else if (k < pivotIndex) {
+            return quickselect(nums, left, pivotIndex - 1, k);
+        } else {
+            return quickselect(nums, pivotIndex + 1, right, k);
+        }
+    }
+
+    private static int partition(int[] nums, int left, int right) {
+        // 무작위 피벗 선택
+        int pivotIndex = left + rand.nextInt(right - left + 1);
+        int pivotValue = nums[pivotIndex];
+        swap(nums, pivotIndex, right);
+        int storeIndex = left;
+
+        for (int i = left; i < right; i++) {
+            if (nums[i] < pivotValue) {
+                swap(nums, i, storeIndex);
+                storeIndex++;
+            }
+        }
+        swap(nums, storeIndex, right);
+        return storeIndex;
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
